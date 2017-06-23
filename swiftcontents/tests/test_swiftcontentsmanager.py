2a41afdc6d64f3357b20d921077581b7d9e5945f
Ian Stuart <ian.stuart@ed.ac.uk>
2017-06-23 16:17:46 +0100

    Add a class to test the internal Content Manager tests

---
+++ swiftcontents/tests/test_swiftcontentsmanager.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+from pprint import pprint
+
+from swiftcontents.ipycompat import TestContentsManager
+
+from swiftcontents import SwiftContentsManager
+from tempfile import TemporaryDirectory
+from tornado.web import HTTPError
+
+# Basic concept:
+# Each file is an object
+# Each directory is an object
+# Each object's "path" is the full directory-path for that object (thus faking
+#   a directory structure
+# When an object is created, we ensure all intermediate directory objects exist
+#   and if they don't, we create them
+# When we delete an object, we ensure that that path lists only one object (ie
+#   that it is a file, or an empty directory
+# When we rename a file, we can rename that file
+# When we rename a directory, we need to rename every object that contains that
+#   path-part
+class Test_SwiftContentsManager(TestContentsManager):
+
+    def setUp(self):
+        """
+        Note: this test requires a bunch of environment variables set, and
+        is written to work in a Docker image, against the UofE 'Horizon' server
+        """
+        self.contents_manager = SwiftContentsManager()
+
+# This needs to be removed or else we'll run the main IPython tests as well.
+del TestContentsManager
